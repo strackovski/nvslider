36c42862c0f606da43e8f7f4a839f66ce154c148
strackovski <vladimir.strackovski@gmail.com>
2014-07-25 23:14:25 +0000

    Add new version.

--- src/jquery.nvslider.js
+++ src/jquery.nvslider.js
@@ -1,3 +1,23 @@
+/* ===================================================
+ * nv/Slider JavaScript Library
+ * http://github.com/strackovski/nvslider
+ * http://www.nv3.org/nvslider
+ * ===================================================
+ * Copyright 2014 Vladimir Stračkovski <vlado@nv3.org>
+ *
+ * Licensed under the MIT License;
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://choosealicense.com/licenses/mit/
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ========================================================== */
+
 /*global $:false, jQuery:false */
 /*global console:false */
 /*jshint unused:false*/
@@ -8,44 +28,43 @@
     $.nvslider = function (el, options) {
         var base = this;
         base.el = el;
-        base.$el = $(el).addClass('nvs-ul clrfix').wrap('<div class="nvs-wrap"></div>');
+        base.$el = $(el).addClass('nvs-ul').wrap('<div class="nvs-wrap"></div>');
 
+        //Init gets called first
         base.init = function () {
 
-            //extend options
+            // Extend options
             base.options = $.extend({}, $.fn.nvslider.defaults, options);
 
-            //add class to wrapper based on 'orientation' option
+            // Add class to wrapper based on 'orientation' option
             base.$wrap = base.$el.parent().closest('div.nvs-wrap').addClass('nvs-wrapper-' + base.options.orientation);
 
-            //cache existing DOM elements
-            //base.$children = base.$el.children('li').addClass('nvs-li-elem');
+            // Cache existing DOM elements
             base.$children = base.$wrap.find('.nvs-ul > *').addClass('nvs-li-elem');
             base.$grandchildren = base.$wrap.find('.nvs-li-elem > *');
 
-            //height for future reference
+            // Height for future reference
             base.pureHeight = parseInt(base.options.height, 10);
 
-            //call setup
+            // Call setup()
             base.setup();
         };
 
+        // General setup
         base.setup = function () {
-
-            //get number of rows if orientation == portrait
-            //else there can only be 1 row
+            // If orientation == landscape, override rows - there can only be 1
             if (base.options.orientation === 'landscape') {
                 base.options.rows = 1;
             }
 
-            //how many items to slide for
-            //in portrait orientation, max number of slideFor is number of rows
+            // How many items to slide for
+            // In portrait orientation, max number of slideFor is number of rows
             if (base.options.orientation === 'portrait') {
                 if (base.options.slideFor > base.options.rows) {
                     base.options.slideFor = base.options.rows;
                 }
             }
-            //handling 'themes'
+            // Add different classes to inner elements based on 'theme'
             if (base.$grandchildren.length > 0) {
                 if (base.options.theme === 'default') {
                     base.$grandchildren.addClass('nvs-span-default');
@@ -59,7 +78,7 @@
                 base.$wrap.addClass('nvs-wrapper-gallery');
             }
 
-            //add navigation
+            // Create navigation
             if (base.options.orientation === 'landscape') {
                 base.$wrap.prepend('<div class="nvs-nav-left nvs-nav"><i class="fa fa-angle-left"></i></div>');
                 base.$wrap.prepend('<div class="nvs-nav-right nvs-nav"><i class="fa fa-angle-right"></i></div>');
@@ -68,26 +87,30 @@
                 base.$wrap.prepend('<div class="nvs-nav-down nvs-nav"><i class="fa fa-angle-down"></i></div>');
             }
 
+            // Cache elements for later use
             base.$nav = base.$wrap.find('.nvs-nav');
             base.$navLeft = base.$wrap.find('.nvs-nav-left');
             base.$navRight = base.$wrap.find('.nvs-nav-right');
             base.$navUp = base.$wrap.find('.nvs-nav-up');
             base.$navDown = base.$wrap.find('.nvs-nav-down');
 
-            //call nav
+            // Call nav()
             base.nav();
         };
 
+        // Set up navigation
         base.nav = function () {
-
+            // Set navigation classes (style)
             base.$navLeft.addClass(base.options.leftArrowClass);
             base.$navRight.addClass(base.options.rightArrowClass);
             base.$navUp.addClass(base.options.topArrowClass);
             base.$navDown.addClass(base.options.bottomArrowClass);
 
+            // Position navigation controls
             var marginTop = (base.pureHeight / 2 - (base.$navLeft.height() / 2)),
                 marginLeft = (base.$wrap.width() / 2 - (base.$navUp.width() / 2));
 
+            // Adjust appearance according to 'theme'
             if (base.options.theme === 'gallery') {
                 base.$nav.css({background: '#000', opacity: 0.8, color: '#fff'});
             }
@@ -100,40 +123,40 @@
                 base.$nav.css({marginTop: marginTop});
             }
 
+            // Call css()
             base.css();
         };
 
         base.css = function () {
-
             var width_in_percents = 100 / base.options.showItems + '%';
             base.$children.css({height: base.options.height});
             base.$wrap.css({height: base.pureHeight * base.options.rows});
             base.$wrap.css(base.options.containerCss);
-            base.$grandchildren.css({display: 'inline-block'});
+            //base.$grandchildren.css({display: 'inline-block'});
             base.$children.width(width_in_percents);
 
             base.clicks();
         };
 
         base.clicks = function () {
-
-            //make sure the slider stops when there are no more items to show
+            // Make sure the slider stops when there are no more items to show
             var totalChildren = base.$children.length,
                 safeLimit = -(totalChildren - base.options.showItems) * (100 / base.options.showItems),
                 control;
 
-            //the bigger number defines control - when the slider should stop sliding
+            // The bigger number defines control - when the slider should stop sliding
             if (base.options.rows >= base.options.slideFor) {
                 control = base.options.rows;
             } else {
                 control = base.options.slideFor;
             }
 
+            // When to stop sliding
             var safeLimitPortrait = -((Math.ceil(totalChildren / base.options.showItems) - control) * base.pureHeight),
                 leftOffset = 0,
                 topOffset = 0;
 
-            //click handlers
+            // Click handlers
             base.$navDown.on('click', function () {
                 topOffset -= base.pureHeight * base.options.slideFor;
                 if (topOffset < safeLimitPortrait) {
@@ -172,8 +195,8 @@
 
         };
 
+        // Resize for fluid effect
         base.resize = function () {
-
             $(window).resize(function () {
                 var navMarginTop = (base.pureHeight / 2 - (base.$nav.height() / 2)),
                     navMarginLeft = (base.$wrap.width() / 2 - (base.$nav.width() / 2));
@@ -188,7 +211,7 @@
             });
         };
 
-        //call init, get everything rolling
+        // Call init, get everything rolling
         base.init();
     };
 
@@ -201,6 +224,7 @@
         });
     };
 
+    // Defaults
     $.fn.nvslider.defaults = {
         showItems: 3,
         orientation: 'landscape',
@@ -212,8 +236,7 @@
         rightArrowClass: 'nvs-horizontal-navigation-arrow',
         topArrowClass: 'nvs-vertical-navigation-arrow',
         bottomArrowClass: 'nvs-vertical-navigation-arrow',
-        height: '100px'
+        height: '150px'
     };
-
 
 }(jQuery, window, document));
